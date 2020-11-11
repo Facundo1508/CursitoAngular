543e9bd191050e87375d4f13156f9bde371d7347
Facundo1508 <facundocontreras2012@gmail.com>
2020-11-10 22:00:12 -0300

    aaaaaaaaah

--- Angular/03-pipes/src/app/app.component.ts
+++ Angular/03-pipes/src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+
+  nombre: string = 'Capitan America';
+  arreglo: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI: number = Math.PI;
+  porcentaje: number = 0.234;
+  salario: number = 1234.5;
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 5000,
+    direccion: {
+      calle: 'Primera',
+      casa: 20
+    }
+  };
+
 }
